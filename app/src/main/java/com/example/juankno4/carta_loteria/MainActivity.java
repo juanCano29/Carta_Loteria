6dbbddfabe611b5f3dde95f01f8cd334f450bc66
juanCano29 <juankno4@gmail.com>
2019-02-04 22:19:10 -0600

    Initial commit

---
+++ app/src/main/java/com/example/juankno4/carta_loteria/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.juankno4.carta_loteria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
